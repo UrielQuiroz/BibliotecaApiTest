3c4033aa883ece60c9c0e94b62e067264d802312
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-29 15:59:26 -0600

    Creando primera prueba unitaria para validar primera letra mayuscula

---
+++ TestingBiblioteca/PruebasUnitarias/Validaciones/PrimeraLetraMayusculaAttributePruebas.cs
@@ -0,0 +1,29 @@
+﻿using BibliotecaAPI.Validaciones;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingBiblioteca.PruebasUnitarias.Validaciones
+{
+    [TestClass]
+    public class PrimeraLetraMayusculaAttributePruebas
+    {
+        [TestMethod]
+        public void IsValid_RetornaExitoso_SiValueEsVacio()
+        {
+            //Preparacion
+            var primeraLetraMayusuculaAttribute = new PrimeraLetraMayusculaAttribute();
+            var validationContext = new ValidationContext(new object());
+            var value = string.Empty;
+
+            //Prueba
+            var resultado = primeraLetraMayusuculaAttribute.GetValidationResult(value, validationContext);
+
+            //Verificacion
+            Assert.AreEqual(expected: ValidationResult.Success, actual: resultado);
+        }
+    }
+}
