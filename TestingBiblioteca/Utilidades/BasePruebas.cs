28041289ce85e36eabf49eb0ffa65f55483e7529
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-29 16:26:35 -0600

    Clase auxiliar para trabajar con EF Core y Controladores

---
+++ TestingBiblioteca/Utilidades/BasePruebas.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BibliotecaAPI.Datos;
+using BibliotecaAPI.Utilidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingBiblioteca.Utilidades
+{
+    public class BasePruebas
+    {
+        protected ApplicationDbContext ConstruirContext(string nombreBD)
+        {
+            var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(nombreBD).Options;
+
+            var dbContext = new ApplicationDbContext(opciones);
+            return dbContext;
+        }
+
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var config = new MapperConfiguration(opciones =>
+            {
+                opciones.AddProfile(new AutoMapperProfiles());
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
