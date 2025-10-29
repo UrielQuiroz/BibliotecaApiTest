3b794b0b2faefcdd3f041d27eb71c03acc20d297
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-29 16:05:03 -0600

    DataRow - . Ejecutando una Misma Prueba con Distintos Valores

--- TestingBiblioteca/PruebasUnitarias/Validaciones/PrimeraLetraMayusculaAttributePruebas.cs
+++ TestingBiblioteca/PruebasUnitarias/Validaciones/PrimeraLetraMayusculaAttributePruebas.cs
@@ -12,12 +12,14 @@ namespace TestingBiblioteca.PruebasUnitarias.Validaciones
     public class PrimeraLetraMayusculaAttributePruebas
     {
         [TestMethod]
-        public void IsValid_RetornaExitoso_SiValueEsVacio()
+        [DataRow("")]
+        [DataRow("    ")]
+        [DataRow(null)]
+        public void IsValid_RetornaExitoso_SiValueEsVacio(string value)
         {
             //Preparacion
             var primeraLetraMayusuculaAttribute = new PrimeraLetraMayusculaAttribute();
             var validationContext = new ValidationContext(new object());
-            var value = string.Empty;
 
             //Prueba
             var resultado = primeraLetraMayusuculaAttribute.GetValidationResult(value, validationContext);
