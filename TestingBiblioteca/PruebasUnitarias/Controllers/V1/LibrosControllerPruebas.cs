f28a617c728dccd2760172583e8995dc264bace8
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-30 15:33:47 -0600

    Probando un Método que usa el HttpContext

---
+++ TestingBiblioteca/PruebasUnitarias/Controllers/V1/LibrosControllerPruebas.cs
@@ -0,0 +1,38 @@
+﻿using BibliotecaAPI.Controllers.V1;
+using BibliotecaAPI.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.OutputCaching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingBiblioteca.Utilidades;
+
+namespace TestingBiblioteca.PruebasUnitarias.Controllers.V1
+{
+    [TestClass]
+    public class LibrosControllerPruebas : BasePruebas
+    {
+        [TestMethod]
+        public async Task Get_RetornaCeroLibros_CuandoNoHayLibros()
+        {
+            //Preparacion
+            var nombreBD = Guid.NewGuid().ToString();
+            var context = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+            IOutputCacheStore outputCacheStore = null!;
+
+            var controller = new LibrosController(context, mapper, outputCacheStore);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var paginacionDTO = new PaginacionDTO(1, 1);
+
+            //Prueba
+            var respuesta = await controller.Get(paginacionDTO);
+
+            //Verificacion
+            Assert.AreEqual(expected: 0, actual: respuesta.Count());
+        }
+    }
+}
