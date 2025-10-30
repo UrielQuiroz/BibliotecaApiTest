0cbf3fe076e8600100cdc5b4b16ab8b233956f9c
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-30 16:29:42 -0600

    UsuariosControllerPruebas => Setup Preparación para Probar Login y Registro

---
+++ TestingBiblioteca/PruebasUnitarias/Controllers/V1/UsuariosControllerPruebas.cs
@@ -0,0 +1,56 @@
+﻿using BibliotecaAPI.Controllers.V1;
+using BibliotecaAPI.Entidades;
+using BibliotecaAPI.Servicios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingBiblioteca.Utilidades;
+
+namespace TestingBiblioteca.PruebasUnitarias.Controllers.V1
+{
+    [TestClass]
+    public class UsuariosControllerPruebas : BasePruebas
+    {
+        private string nombreBD = Guid.NewGuid().ToString();
+        private UserManager<Usuario> userManager = null!;
+        private SignInManager<Usuario> signInManager = null!;
+        private UsuariosController controller = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var context = ConstruirContext(nombreBD);
+
+            userManager = Substitute.For<UserManager<Usuario>>(
+                Substitute.For<IUserStore<Usuario>>(), null, null, null, null, null, null, null, null);
+
+            var miConfiguracion = new Dictionary<string, string>
+                {
+                    {
+                        "llavejwt", "askjdansjkdansjkdNJKANSDJKANSDJKASNDAJKSNDJ"
+                    }
+                };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(miConfiguracion!)
+                .Build();
+
+            var contextAccessor = Substitute.For<IHttpContextAccessor>();
+            var userClaimsFactory = Substitute.For<IUserClaimsPrincipalFactory<Usuario>>();
+
+            signInManager = Substitute.For<SignInManager<Usuario>>(userManager, contextAccessor, userClaimsFactory, null, null, null, null);
+
+            var servicioUsuarios = Substitute.For<IServicioUsuarios>();
+
+            var mapper = ConfigurarAutoMapper();
+
+            controller = new UsuariosController(userManager, configuration, signInManager, servicioUsuarios, context, mapper);
+        }
+    }
+}
