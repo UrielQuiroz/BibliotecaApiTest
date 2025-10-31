9bea5c0b110a5fd1b801c393c5fa9c6cf766d889
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-31 15:59:06 -0600

    Probando que un Filtro se Ejecuta

---
+++ TestingBiblioteca/PruebasDeIntegracion/Controllers/V1/LibrosControllerPruebas.cs
@@ -0,0 +1,28 @@
+﻿using BibliotecaAPI.DTOs;
+using System.Net;
+using TestingBiblioteca.Utilidades;
+
+namespace TestingBiblioteca.PruebasDeIntegracion.Controllers.V1
+{
+    [TestClass]
+    public class LibrosControllerPruebas : BasePruebas
+    {
+        private readonly string url = "/api/v1/libros";
+        private string nombreBD = Guid.NewGuid().ToString();
+
+        [TestMethod]
+        public async Task Post_Devuelve400_CuandoAutoresIdsEsVacio()
+        {
+            //Preparacion
+            var factory = ConstruirWebApplicationFactory(nombreBD);
+            var cliente = factory.CreateClient();
+            var libroCreacionDTO = new LibroCreateDTO { Titulo  = "Libro" };
+
+            //Prueba
+            var respuesta = await cliente.PostAsJsonAsync(url, libroCreacionDTO);
+
+            //Verificacion
+            Assert.AreEqual(expected: HttpStatusCode.BadRequest, actual: respuesta.StatusCode);
+        }
+    }
+}
